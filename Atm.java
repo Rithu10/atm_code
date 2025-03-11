a1baf878548a4cb66388b67177e65fa0162f7f9d
Rithu10 <rithurithwik2208@gmail.com>
2025-03-11 12:54:33 +0530

    Add files via upload

---
+++ Atm.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+class Atm
+	{
+	   void userBank
+	{
+  	   Sacnner sd =new Scanner(System.in);
+	   System.out.println("Enter the option");
+	   System.out.println("1.Withdraw");
+	   System.out.println("2.Deposit");
+	   System.out.println("3.Pinnumber");
+	   System.out.Println("4.Cashbalance");
+	   int option=s.nextInt();
+	Switch(option)
+	{
+	  case 1:
+		withDraw();
+		Break;
+	  case 2:
+ 		cashDeposit();
+		Break;
+	  case 3:
+		pinNumber();
+		Break;
+	  case 4:
+		cashBalance();
+		Break;
+	      }
+	}
+   	  Scanner sd=new Sacnner(system.in);
+	  Void withDraw()
+	 {
+	   System.out.println("Enter the Amount");
+	   int amount=s.nextInt();
+	   System.out.println(amt);
+	}
+	  Void cashDeposit()
+	 {
+	   System.out.println("Enter the Amount");
+	   int amount=s.nextInt();
+	}
+	  Void pinNumber()
+	 {
+	   System.out.println("Enter the number");
+	   int amount=s.nextInt();
+	}
+	  Void cashBalance()
+	 {
+	   System.out.println("Enter the Amount");
+	   int amount=s.nextInt();
+	}
+	  Public Static void main(String ags[])
+		{
+		   Atm a=new Atm
+		   a.userBank()
+		}
+	}
+	  
